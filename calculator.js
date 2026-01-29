be51f0e5898ba6e25ae78ef2298e805cf155900f
AMN-47 <akhilnagar57@gmail.com>
2026-01-29 16:36:13 +0200

    added functions

--- calculator.js
+++ calculator.js
@@ -5,5 +5,8 @@ document.addEventListener("DOMContentLoaded" ,function(){
     let decimal = document.querySelector(".decimal");
 
     let numbers = document.querySelectorAll(".number");
-    let operators = document.querySelectorAll(".operator")
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
 })
